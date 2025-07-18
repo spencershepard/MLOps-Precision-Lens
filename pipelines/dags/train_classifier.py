74a3a59539374f930605c76ce5ee7bf573e8d088
Spencer Shepard <spencershepard@hotmail.com>
2025-07-18 16:36:40 -0700

    detect new data in s3

--- pipelines/dags/train_classifier.py
+++ pipelines/dags/train_classifier.py
@@ -1,25 +1,52 @@
 from airflow import DAG
-from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
-from datetime import datetime
+from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
+from airflow.providers.amazon.aws.sensors.s3 import S3KeysUnchangedSensor
+from datetime import datetime, timedelta
+import os
+from airflow.models.connection import Connection
+
+
+conn = Connection(
+    conn_id="my-aws-connection",
+    conn_type="aws",
+    login=os.getenv("AWS_ACCESS_KEY_ID"),  # Reference to AWS Access Key ID
+    password=os.getenv("AWS_SECRET_ACCESS_KEY"),  # Reference to AWS Secret Access Key
+    extra={
+        "region_name": os.getenv("AWS_REGION"),
+    },
+)
 
 with DAG(
-    dag_id='train_classifier',
+    dag_id='train_ml_model',  # Renamed to be more generic as it now has multiple triggers
     start_date=datetime(2025, 1, 1),
+    schedule_interval=None,  # still supports manual triggering
     catchup=False,
+    tags=['ml', 'training'],
+    default_args={
+        'retries': 1,
+        'retry_delay': timedelta(minutes=5),
+    }
 ) as dag:
 
+    # Sensor to detect new data in S3 bucket, waiting for changes to stop before triggering training
+    detect_new_data = S3KeysUnchangedSensor(
+        task_id='detect_new_data',
+        bucket_name=os.getenv('BUCKET_NAME'),
+        bucket_key='*.png',  # pattern based on your data files
+        wildcard_match=True,  # Enable wildcard matching for detecting multiple files
+        poke_interval=1 * 60,  # Check every seconds
+        inactivity_period=10,  # Wait for inactivity before triggering
+    )
+
     train_model = KubernetesPodOperator(
-        task_id="train_classifier_task",
-        name="classifier-training-job",
+        task_id="train_model",
+        name="ml-training-job",
         namespace="default",
-        image="ghcr.io/spencershepard/mlops-precision-lens/classifier-train:develop", 
+        image="ghcr.io/spencershepard/mlops-precision-lens/classifier-train:develop",
+        cmds=["python", "train.py"],
         get_logs=True,
         is_delete_operator_pod=True,
-        in_cluster=False,  # If Airflow is running inside the cluster, we can use a service account token (requires RBAC setup)
-        kubernetes_conn_id="my-cluster", # This needs to be set up in Airflow connections (UI)
-        env_from=[
-            {"configMapRef": {"name": "pl-config"}},
-            {"secretRef": {"name": "pl-secrets"}}
-        ],
-        image_pull_policy="Always", # temporary 
     )
+
+    # Define task dependencies
+    detect_new_data >> train_model
