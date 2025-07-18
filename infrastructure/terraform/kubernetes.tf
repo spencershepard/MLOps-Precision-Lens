c001e0fe00b3e5b1d176b285f2923ca5dc35c198
Spencer Shepard <spencershepard@hotmail.com>
2025-07-18 14:44:30 -0700

    fix env var bug from extra quotes

--- infrastructure/terraform/kubernetes.tf
+++ infrastructure/terraform/kubernetes.tf
@@ -13,17 +13,28 @@ data "local_file" "config" {
 }
 
 locals {
-  # Parse secrets.env into a map
+  # Parse secrets.env into a map, trimming whitespace and ignoring empty lines
   secrets = {
-    for line in split("\n", data.local_file.secrets.content) :
-    split("=", line)[0] => split("=", line)[1]
-    if length(split("=", line)) == 2
+    for line in compact(split("\n", data.local_file.secrets.content)) :
+    trimspace(split("=", line)[0]) => (
+      # Remove surrounding quotes if present
+      length(regexall("^\"(.*)\"$", trimspace(split("=", line)[1]))) > 0 
+        ? replace(trimspace(split("=", line)[1]), "/^\"(.*)\"$/", "$1") 
+        : trimspace(split("=", line)[1])
+    )
+    if length(split("=", line)) == 2 && trimspace(line) != ""
   }
-  # Parse config.env into a map
+  
+  # Parse config.env into a map, trimming whitespace and ignoring empty lines
   config = {
-    for line in split("\n", data.local_file.config.content) :
-    split("=", line)[0] => split("=", line)[1]
-    if length(split("=", line)) == 2
+    for line in compact(split("\n", data.local_file.config.content)) :
+    trimspace(split("=", line)[0]) => (
+      # Remove surrounding quotes if present
+      length(regexall("^\"(.*)\"$", trimspace(split("=", line)[1]))) > 0 
+        ? replace(trimspace(split("=", line)[1]), "/^\"(.*)\"$/", "$1") 
+        : trimspace(split("=", line)[1])
+    )
+    if length(split("=", line)) == 2 && trimspace(line) != ""
   }
 }
 
@@ -41,4 +52,26 @@ resource "kubernetes_config_map" "pl_config" {
     namespace = "default"
   }
   data = local.config
+}
+
+output "config_map_debug" {
+  value = {
+    for key, value in local.config : key => {
+      value       = value
+      length      = length(value)
+      byte_length = length(jsonencode(value)) - 2  # Subtract 2 for the quotes
+      json_debug  = jsonencode(value)
+    }
+  }
+  sensitive = false 
+}
+
+output "secrets_debug" {
+  value = {
+    for key, value in local.secrets : key => {
+      length      = length(value)
+      byte_length = length(jsonencode(value)) - 2  # Subtract 2 for the quotes
+    }
+  }
+  sensitive = true
 }
