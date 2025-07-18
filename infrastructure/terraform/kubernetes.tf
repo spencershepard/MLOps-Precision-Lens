5a6e335bb80c239509924463ce719113f383eb21
Spencer Shepard <spencershepard@hotmail.com>
2025-07-18 16:36:40 -0700

    detect new data in s3

--- infrastructure/terraform/kubernetes.tf
+++ infrastructure/terraform/kubernetes.tf
@@ -12,6 +12,9 @@ data "local_file" "config" {
   filename = "${path.module}/../../config.env"
 }
 
+## I'm not super confident in this env file parsing, so if trying to debug
+## check the output debug blocks.  Also you could overwrite with 
+## `kubectl create secret generic pl-secrets --from-env-file=secrets.env`
 locals {
   # Parse secrets.env into a map, trimming whitespace and ignoring empty lines
   secrets = {
