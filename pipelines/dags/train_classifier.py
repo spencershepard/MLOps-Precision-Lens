26796c9d805ee9e511dfa76b23fd17e2b8663d8c
Spencer Shepard <spencershepard@hotmail.com>
2025-07-18 16:50:10 -0700

    Update train_classifier.py

--- pipelines/dags/train_classifier.py
+++ pipelines/dags/train_classifier.py
@@ -1,6 +1,6 @@
 from airflow import DAG
 from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
-from airflow.providers.amazon.aws.sensors.s3 import S3KeysUnchangedSensor
+from airflow.providers.amazon.aws.sensors.s3 import S3KeySensor
 from datetime import datetime, timedelta
 import os
 from airflow.models.connection import Connection
@@ -28,14 +28,13 @@ with DAG(
 ) as dag:
 
     # Sensor to detect new data in S3 bucket, waiting for changes to stop before triggering training
-    detect_new_data = S3KeysUnchangedSensor(
-        aws_conn_id='my-aws-connection',
+    detect_new_data = S3KeySensor(
         task_id='detect_new_data',
+        aws_conn_id='my-aws-connection',
         bucket_name=os.getenv('BUCKET_NAME'),
         bucket_key='*.png',  # pattern based on your data files
         wildcard_match=True,  # Enable wildcard matching for detecting multiple files
         poke_interval=1 * 60,  # Check every seconds
-        inactivity_period=10,  # Wait for inactivity before triggering
     )
 
     train_model = KubernetesPodOperator(
