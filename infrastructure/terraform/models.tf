57c8e5e924bc85d8d94bd1e944596263b7e2f043
spencershepard <spencershepard@hotmail.com>
2025-09-14 09:40:34 -0700

    Update models.tf

--- infrastructure/terraform/models.tf
+++ infrastructure/terraform/models.tf
@@ -132,3 +132,98 @@ resource "kubernetes_config_map" "models_config" {
   data = local.config
 }
 
+resource "kubernetes_deployment_v1" "anomaly" {
+  metadata {
+    name      = "anomaly"
+    namespace = kubernetes_namespace.models.metadata[0].name
+  }
+
+  depends_on = [ kubernetes_namespace.models, kubernetes_persistent_volume_claim.s3cache ]
+
+  spec {
+    replicas = 1
+
+    selector {
+      match_labels = {
+        app = "models"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "models"
+        }
+      }
+
+      spec {
+        node_selector = {
+          "kubernetes.io/hostname" = "docker-desktop"
+        }
+
+        volume {
+          name = "s3cache"
+          persistent_volume_claim {
+            claim_name = kubernetes_persistent_volume_claim.s3cache.metadata[0].name
+          }
+        }
+
+        container {
+          name  = "anomaly"
+          image = "ghcr.io/spencershepard/mlops-precision-lens/anomaly_detection:develop"
+          args = [ "python", "-u", "predict.py" ]
+
+          volume_mount {
+            name      = "s3cache"
+            mount_path = "/mnt/s3cache"
+          }
+
+          port {
+            container_port = 8000
+          }
+
+          env {
+            name  = "ENVIRONMENT"
+            value = var.environment
+          }
+
+          env_from {
+            config_map_ref {
+              name = "models-config"
+            }
+          }
+
+          env_from {
+            secret_ref {
+              name = "models-secrets"
+            }
+          }
+
+        }
+
+      }
+    }
+  }
+}
+
+
+resource "kubernetes_service" "anomaly" {
+  metadata {
+    name      = "anomaly"
+    namespace = kubernetes_namespace.models.metadata[0].name
+    annotations = {
+      "tailscale.com/expose" = "true"
+    }
+  }
+  spec {
+    selector = {
+      app = "models"
+    }
+    port {
+      port        = 8000
+      target_port = 8000
+      protocol    = "TCP"
+    }
+    type = "ClusterIP"
+  }
+}
